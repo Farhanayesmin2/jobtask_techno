dd59ae9628146f237f4bc27a7c605063e94acd17
Farhanayesmin2 <farhanayesmin550@gmail.com>
2024-03-03 00:54:55 +0600

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
   return (
     <>
       <h1>Its a job task.</h1>
-      <a href="https://example.com" target="_blank" rel="noreferrer">Link</a> {/* No JSX target _blank rule */}
+      <h1  className="text-red-600" >Hello world!</h1>
     </>
   );
 }
