d8361591ce59a16916f7da29c38729398ae38177
Farhanayesmin2 <farhanayesmin550@gmail.com>
2024-03-03 00:48:20 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# React + Vite
+
+## Firstly i install the react vite, after that install the npm install ,tailwind css.
+1.React with vite
+2.Npm install
+3.Tailwind Css
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <>
+      <h1>Its a job task.</h1>
+      <a href="https://example.com" target="_blank" rel="noreferrer">Link</a> {/* No JSX target _blank rule */}
+    </>
+  );
+}
+
+export default App;
