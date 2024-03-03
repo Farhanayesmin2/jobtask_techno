28f486e6693552f6b0d0adccb69d6c475bde33bd
Farhanayesmin2 <farhanayesmin550@gmail.com>
2024-03-03 10:31:49 +0600

    Intall react router dom and setup

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # React + Vite
 
 ## Firstly i install the react vite, after that install the npm install ,tailwind css.
-1.React with vite
-2.Npm install
-3.Tailwind Css
+1. React with vite
+2. Npm install
+3. Tailwind Css
+4. React-Router-Dom - npm install react router dom
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { RouterProvider } from "react-router-dom"
+import router from "./Routes/router"
+
 function App() {
+ 
 
   return (
-    <>
-      <h1>Its a job task.</h1>
-      <h1  className="text-red-600" >Hello world!</h1>
-    </>
-  );
+    <div className=' mx-auto xl:w-[1600px] max-w-screen-2xl'>
+    <RouterProvider router={router}></RouterProvider>
+  </div>
+  )
 }
 
-export default App;
+export default App
