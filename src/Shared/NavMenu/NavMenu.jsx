66fb665020c0eb0104cf483aaf1de7781106e29a
Farhanayesmin2 <farhanayesmin550@gmail.com>
2024-03-03 13:15:05 +0600

    Add the arrow icon

--- src/Shared/NavMenu/NavMenu.jsx
+++ src/Shared/NavMenu/NavMenu.jsx
@@ -2,6 +2,7 @@
 import  { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../../public/svg.png";
+import { RiArrowDropDownLine } from "react-icons/ri";
 
 const Navmenu = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
@@ -19,9 +20,59 @@ const Navmenu = () => {
         
         {/* Menu items */}
         <nav className="hidden lg:flex items-start gap-5 ml-5">
-          <Link to='/' className="text-black text-lg font-normal font-['Open Sans'] cursor-pointer hover:underline">Product</Link>
-          <Link to='/solutions' className="text-black text-lg font-normal font-['Open Sans'] cursor-pointer hover:underline">Solutions</Link>
-          <Link to='/resources' className="text-black text-lg font-normal font-['Open Sans'] cursor-pointer hover:underline">Resources</Link>
+             {/* Product */}
+             <div className="relative flex items-center">
+            <Link to='/' className="text-black text-lg font-normal font-['Open Sans'] cursor-pointer hover:underline">Product</Link>
+            <RiArrowDropDownLine className="text-black text-3xl cursor-pointer ml-0" onClick={toggleDropdown} />
+
+            {/* Dropdown Content */}
+            {isDropdownOpen && (
+              <ul className="absolute left-0 mt-2 w-40 bg-white shadow-lg rounded-md overflow-hidden z-10">
+                <li>
+                  <Link to='/service1' className="block px-4 py-2 text-sm text-gray-700 hover:text-cyan-700 hover:font-bold">Service 1</Link>
+                </li>
+                <li>
+                  <Link to='/service2' className="block px-4 py-2 text-sm text-gray-700 hover:text-cyan-700 hover:font-bold">Service 2</Link>
+                </li>
+              </ul>
+            )}
+          </div>
+          {/* Solutions */}
+          <div className="relative flex items-center">
+            <Link to='/' className="text-black text-lg font-normal font-['Open Sans'] cursor-pointer hover:underline">Solutions</Link>
+            <RiArrowDropDownLine className="text-black text-3xl cursor-pointer ml-0" onClick={toggleDropdown} />
+
+            {/* Dropdown Content */}
+            {isDropdownOpen && (
+              <ul className="absolute left-0 mt-2 w-40 bg-white shadow-lg rounded-md overflow-hidden z-10">
+                <li>
+                  <Link to='/service1' className="block px-4 py-2 text-sm text-gray-700 hover:text-cyan-700 hover:font-bold">Service 1</Link>
+                </li>
+                <li>
+                  <Link to='/service2' className="block px-4 py-2 text-sm text-gray-700 hover:text-cyan-700 hover:font-bold">Service 2</Link>
+                </li>
+                
+              </ul>
+            )}
+          </div>
+          {/* Resources */}
+          <div className="relative flex items-center">
+            <Link to='/' className="text-black text-lg font-normal font-['Open Sans'] cursor-pointer hover:underline">Resources</Link>
+            <RiArrowDropDownLine className="text-black text-3xl cursor-pointer ml-0" onClick={toggleDropdown} />
+
+            {/* Dropdown Content */}
+            {isDropdownOpen && (
+              <ul className="absolute left-0 mt-2 w-40 bg-white shadow-lg rounded-md overflow-hidden z-10">
+                <li>
+                  <Link to='/service1' className="block px-4 py-2 text-sm text-gray-700 hover:text-cyan-700 hover:font-bold">Service 1</Link>
+                </li>
+                <li>
+                  <Link to='/service2' className="block px-4 py-2 text-sm text-gray-700 hover:text-cyan-700 hover:font-bold">Service 2</Link>
+                </li>
+                
+              </ul>
+            )}
+          </div>
           <Link to='/enterprise' className="text-black text-lg font-normal font-['Open Sans'] cursor-pointer hover:underline">Enterprise</Link>
           <Link to='/pricing' className="text-black text-lg font-normal font-['Open Sans'] cursor-pointer hover:underline">Pricing</Link>
         </nav>
