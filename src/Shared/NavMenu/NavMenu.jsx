b54de21bc1c4069d93fa140d760b988f4d6bb5d6
Farhanayesmin2 <farhanayesmin550@gmail.com>
2024-03-03 15:42:22 +0600

    Navmenu global icon add,button etc

--- src/Shared/NavMenu/NavMenu.jsx
+++ src/Shared/NavMenu/NavMenu.jsx
@@ -3,6 +3,7 @@ import  { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../../public/svg.png";
 import { RiArrowDropDownLine } from "react-icons/ri";
+import { AiOutlineGlobal } from "react-icons/ai";
 
 const Navmenu = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
@@ -50,8 +51,7 @@ const Navmenu = () => {
                 </li>
                 <li>
                   <Link to='/service2' className="block px-4 py-2 text-sm text-gray-700 hover:text-cyan-700 hover:font-bold">Service 2</Link>
-                </li>
-                
+                </li>     
               </ul>
             )}
           </div>
@@ -79,26 +79,29 @@ const Navmenu = () => {
       </div>
 
       {/* Right side with login button, global icon, and language change */}
-      <div className="flex items-center">
-        {/* Mobile Menu Icon */}
-        <div className="lg:hidden cursor-pointer" onClick={toggleMobileMenu}>
-          {/* Insert your mobile menu icon here */}
-        </div>
+        {/* Right side with global icon, language change, contact sales, login button, and sign up button */}
+        <div className="flex items-center">
+        {/* Global icon */}
+        <div className="mr-5 flex items-center">
+          <AiOutlineGlobal className="w-6 h-6" />
+       
+
+        {/* Language change */}
+        <select className="text-black  bg-white outline-0 text-lg font-normal font-['Open Sans'] cursor-pointer mr-5">
+          <option value="en">EN</option>
+          <option value="bn">BN</option>
+          {/* Add more language options as needed */}
+        </select>
+        
+        {/* Contact sales */}
+        <div className="mr-5">Contact Sales</div>
 
         {/* Login button */}
         <button className="text-black text-lg font-normal font-['Open Sans'] cursor-pointer hover:underline mr-5">Login</button>
 
-        {/* Global icon (example) */}
-        <div className="w-6 h-6 mr-5">
-          {/* Insert your global icon component here */}
-        </div>
-
-        {/* Language change (example) */}
-        <select className="text-black text-lg font-normal font-['Open Sans'] cursor-pointer hover:underline">
-          <option value="en">English</option>
-          <option value="fr">French</option>
-          {/* Add more language options as needed */}
-        </select>
+        {/* Sign up button */}
+        <button className="text-black text-lg font-normal font-['Open Sans'] cursor-pointer hover:underline">Sign Up Free</button>
+      </div>
       </div>
 
       {/* Dropdown Content */}
