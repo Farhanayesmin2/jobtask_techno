689d565dd0dac3cf5578a683e5626fc2c2dab1a9
Farhanayesmin2 <farhanayesmin550@gmail.com>
2024-03-04 12:41:46 +0600

    modify the trusted section

--- src/Section/TrustedSection/TrustedSection.jsx
+++ src/Section/TrustedSection/TrustedSection.jsx
@@ -7,21 +7,21 @@ function TrustedSection() {
   return (
     <section className="min-h-screen">
          <div className="max-w-4xl mx-auto">
-        <h2 className="text-[#050038] font-[18px] text-center mb-8">Trusted by 45M+ Users</h2>
+        <h2 className="text-gray-500 text-md text-center mb-8">Trusted by 45M+ Users</h2>
         <div className="grid grid-cols-5 gap-5">
-          <div className="bg-white border  p-3 flex justify-center items-center">
+          <div className="bg-white   p-3 flex justify-center items-center">
             <img src={img1} alt="SVG 1" className="max-w-full " />
           </div>
-          <div className="bg-white border  p-3 flex justify-center items-center">
+          <div className="bg-white   p-3 flex justify-center items-center">
             <img src={img2} alt="SVG 2" className="max-w-full " />
           </div>
-          <div className="bg-white border  p-3 flex justify-center items-center">
+          <div className="bg-white   p-3 flex justify-center items-center">
             <img src={img3} alt="SVG 3" className="max-w-full " />
           </div>
-          <div className="bg-white border  p-3 flex justify-center items-center">
+          <div className="bg-white   p-3 flex justify-center items-center">
             <img src={img4} alt="SVG 4" className="max-w-full " />
           </div>
-          <div className="bg-white border  p-3 flex justify-center items-center">
+          <div className="bg-white   p-3 flex justify-center items-center">
             <img src={img5} alt="SVG 5" className="max-w-full " />
           </div>
         </div>
