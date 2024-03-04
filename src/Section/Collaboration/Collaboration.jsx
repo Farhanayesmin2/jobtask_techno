9e580a076d5375e3125f4fae98a8d0cfb77a63cb
Farhanayesmin2 <farhanayesmin550@gmail.com>
2024-03-04 14:38:06 +0600

    done with mobile responsive in collaborate section

--- src/Section/Collaboration/Collaboration.jsx
+++ src/Section/Collaboration/Collaboration.jsx
@@ -12,27 +12,27 @@ const Collaboration = () => {
                         Collaborate without<br />
                         constraints
                     </h1>
-                    <div className="bg-[#FFD02F] uppercase p-2 font-sans text-xs text-[#090909] absolute object-right-top	-top-10 right-64 transform rotate-12  font-bold px-2 py-1 rounded-full">
-              Your idea starts here
-            </div>
+                    <div className="bg-[#FFD02F] uppercase p-2 font-sans text-xs text-[#090909] absolute object-right-top -top-12 right-12	lg:-top-10 lg:right-64 transform rotate-12  font-bold px-2 py-1 rounded-full">
+                        Your idea starts here
+                    </div>
                 </div>
                 {/* Second child div */}
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                     {/* Three grid text paragraphs */}
-                    <div>
+                    <div className="text-center lg:text-justify">
                         <p className="text-[#050038] text-lg my-2">Free forever</p>
                         <span className="text-[#050038] text-opacity-60 text-sm text-justify	">
                             Our free plan gives you unlimited team <br></br> members, 3 boards, and 300+ expert-<br></br>made templates. Signing up with your <br></br>work email lets you bring in your team <br></br>faster. See our <br></br> <span className="text-[#4262FF]">pricing plans </span>  for more features.
                         </span>
                     </div>
-                    <div>
-                    <p className="text-[#050038] text-lg my-2">Easy integrations</p>
+                    <div className="text-center lg:text-justify">
+                        <p className="text-[#050038] text-lg my-2">Easy integrations</p>
                         <span className="text-[#050038] text-opacity-60 text-sm text-justify	">
                             Miro has 100+ powerful integrations <br></br> with tools you already use like G <br></br> Suite, Slack, and Jira, so your <br></br> workflow is seamless. View the full <br></br> list in our <span className="text-[#4262FF]">Marketplace</span>.
                         </span>
                     </div>
-                    <div>
-                    <p className="text-[#050038] text-lg my-2">Security first</p>
+                    <div className="text-center lg:text-justify">
+                        <p className="text-[#050038] text-lg my-2">Security first</p>
                         <span className="text-[#050038] text-opacity-60 text-sm text-justify	" >
                             We treat your data like you would — <br></br>with the utmost care. We follow <br></br> industry-leading security standards <br></br> and give you tools to protect<br></br> intellectual property. Learn more <br></br> at our <span className="text-[#4262FF]">Trust Center</span>.
                         </span>
