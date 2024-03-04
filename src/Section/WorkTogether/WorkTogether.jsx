6af0fbee9e95b59f0f310bc51841c60bda482813
Farhanayesmin2 <farhanayesmin550@gmail.com>
2024-03-04 15:57:19 +0600

    create the work div

---
+++ src/Section/WorkTogether/WorkTogether.jsx
@@ -0,0 +1,21 @@
+
+
+const WorkTogether = () => {
+    return (
+        <>
+            <div>
+                {/* Text div left side */}
+                <div>
+
+                </div>
+                {/* Image div right side */}
+                <div>
+
+                </div>
+
+            </div>
+        </>
+    );
+};
+
+export default WorkTogether;
