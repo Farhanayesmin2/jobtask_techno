538b19b499fae863e2cbe7c912a638a2079a2e24
Farhanayesmin2 <farhanayesmin550@gmail.com>
2024-03-04 17:59:02 +0600

    connect tool section created

---
+++ src/Section/ConnectTool/ConnectTool.jsx
@@ -0,0 +1,19 @@
+
+
+const ConnectTool = () => {
+    return (
+        <>
+        {/* Image left part */}
+        <div>
+
+        </div>
+        {/* text right part */}
+        <div>
+
+        </div>
+            
+        </>
+    );
+};
+
+export default ConnectTool;
--- src/Section/WorkTogether/WorkTogether.jsx
+++ src/Section/WorkTogether/WorkTogether.jsx
@@ -9,8 +9,6 @@ const WorkTogether = () => {
                 <div>
                     <h1 className="text-[#050038] my-6 text-2xl lg:text-start text-center lg:text-5xl font-bold"> Work together, <br className='lg:hidden'></br>wherever you work.</h1>
                     <div className=' lg:text-start text-center'>
-
-
                         <span className="text-opacity-60 text-md text-[#050038]  lg:text-lg mb-4">
                             In the office, remote, or a mix of the two, with Miro, your team can connect, collaborate, and co-create in one space no matter where you are.
                         </span>
@@ -19,7 +17,6 @@ const WorkTogether = () => {
                         <LearnMore />
                     </div>
                 </div>
-
                 {/* Image div right side */}
                 <div>
                     <img src={workImg} alt="Team collaboration" className="max-w-full h-auto" />
