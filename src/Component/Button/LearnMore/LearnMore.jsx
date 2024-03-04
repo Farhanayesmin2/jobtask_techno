1409faf996580c7155e4a0e015940f0bc7bd5c7c
Farhanayesmin2 <farhanayesmin550@gmail.com>
2024-03-04 17:51:46 +0600

    modify the work section and do it responsive

--- src/Component/Button/LearnMore/LearnMore.jsx
+++ src/Component/Button/LearnMore/LearnMore.jsx
@@ -1,15 +1,11 @@
 import { Link } from 'react-router-dom';
 import { IoArrowForward } from "react-icons/io5";
 
-
-
-
-
 const LearnMore = () => {
     return (
         <>
-            <Link to="#" className="inline-block  text-[#4262FF] px-4 py-2 rounded-md transition duration-300 ease-in-out hover:bg-blue-600 mb-4">
-                <span className="flex items-center">
+            <Link to="/about" className="lg:inline-block  lg:flex-none lg:items-start flex items-center lg:justify-start justify-center text-[#4262FF] mb-4">
+                <span className="flex items-center text-start border-[#4262FF] border-b">
                     Learn More  <IoArrowForward className="ml-2" />
                 </span>
             </Link>
--- src/Section/WorkTogether/WorkTogether.jsx
+++ src/Section/WorkTogether/WorkTogether.jsx
@@ -1,27 +1,31 @@
-import workImg from "../../../public/hybridwork.png.svg"
-import LearnMore from "../../Component/Button/LearnMore/LearnMore";
+import LearnMore from '../../Component/Button/LearnMore/LearnMore';
+import workImg from '../../../public/hybridwork.png.svg';
 
 const WorkTogether = () => {
     return (
-        <>
-            <div className="max-w-5xl mx-auto mt-12">
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center p-8">
-                    {/* Text div left side */}
-                    <div>
-                        <h1 className="text-[#050038] my-6 text-5xl font-bold"> Work together, wherever you work.</h1>
-                        <span className="text-opacity-60 text-lg text-[#050038] md:text-lg mb-4">
+        <div className="max-w-5xl mx-auto mt-12">
+            <div className="grid lg:grid-cols-2 grid-cols-1 gap-8 items-center ">
+                {/* Text div left side */}
+                <div>
+                    <h1 className="text-[#050038] my-6 text-2xl lg:text-start text-center lg:text-5xl font-bold"> Work together, <br className='lg:hidden'></br>wherever you work.</h1>
+                    <div className=' lg:text-start text-center'>
+
+
+                        <span className="text-opacity-60 text-md text-[#050038]  lg:text-lg mb-4">
                             In the office, remote, or a mix of the two, with Miro, your team can connect, collaborate, and co-create in one space no matter where you are.
                         </span>
-                        <LearnMore></LearnMore>
                     </div>
-                   
-                    {/* Image div right side */}
-                    <div>
-                        <img src={workImg} alt="Team collaboration" className="max-w-full h-auto" />
+                    <div className="   my-6">
+                        <LearnMore />
                     </div>
                 </div>
+
+                {/* Image div right side */}
+                <div>
+                    <img src={workImg} alt="Team collaboration" className="max-w-full h-auto" />
+                </div>
             </div>
-        </>
+        </div>
     );
 };
 
