31d0470b16792e6e140694df232ddddb50a09ab7
Farhanayesmin2 <farhanayesmin550@gmail.com>
2024-03-04 19:41:51 +0600

    create the way work section

--- src/Section/WorkTogether/WorkTogether.jsx
+++ src/Section/WorkTogether/WorkTogether.jsx
@@ -8,7 +8,7 @@ const WorkTogether = () => {
                 {/* Text div left side */}
                 <div>
                     <h1 className="text-[#050038] my-6 text-2xl lg:text-start text-center lg:text-5xl font-bold"> Work together, <br className='lg:hidden'></br>wherever you work.</h1>
-                    <div className=' lg:text-start text-center'>
+                    <div className=' lg:px-0 px-12 lg:text-start text-center'>
                         <span className="text-opacity-60 text-md text-[#050038]  lg:text-lg mb-4">
                             In the office, remote, or a mix of the two, with Miro, your team can connect, collaborate, and co-create in one space no matter where you are.
                         </span>
