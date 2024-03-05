6c9ff0ed51749f433f21f78800985362481859f2
Farhanayesmin2 <farhanayesmin550@gmail.com>
2024-03-05 19:53:06 +0600

    i create the footer structure

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,9 +1,16 @@
 
 const Footer = () => {
     return (
-        <div>
-            <h1>This is footer.</h1>
-        </div>
+        <>
+         
+    <div className="grid grid-cols-12 gap-4">
+      <div className="col-span-4 border border-red-500 bg-gray-200">First Column (spanning 4 columns)</div>
+      <div className="col-span-3 border border-blue-500 bg-gray-300">Second Column (spanning 3 columns)</div>
+      <div className="col-span-5 border border-green-500 bg-gray-400">Third Column (spanning 5 columns)</div>
+    </div>
+ 
+
+        </>
     );
 };
 
