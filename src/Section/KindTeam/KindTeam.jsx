7e2f54c7c3f9ee83f337dad938857c11968f372f
Farhanayesmin2 <farhanayesmin550@gmail.com>
2024-03-05 15:40:46 +0600

    added the list items with the react icon

--- src/Section/KindTeam/KindTeam.jsx
+++ src/Section/KindTeam/KindTeam.jsx
@@ -1,30 +1,42 @@
 import BuiltKindTeam from "../../Component/Button/BuiltKindTeam/BuiltKindTeam";
 import LearnMore from "../../Component/Button/LearnMore/LearnMore";
 import RightSideImage from "../../../public/uxdesignwithoutshadow.jpg.svg";
+import { FaCheck } from "react-icons/fa6";
 
 const KindTeam = () => {
     return (
         <>
-        <div className="max-w-5xl mx-auto mt-12 py-8">
-            <h1 className="lg:text-5xl text-2xl lg:text-start  text-center text-[#050038] font-bold mb-4">Built for all kinds of teams</h1>
-            <div className="">
-                <BuiltKindTeam></BuiltKindTeam>
-            </div>
-            <div className="mt-8 grid  lg:grid-cols-3   gap-4">
-                <div className="lg:col-span-1 grid-cols-1 justify-center">
-                    <div className='lg:text-start text-center'>
-                        <p className="text-lg text-[#050038]">Brainstorming</p>
-                        <div className='my-8'>
-                            <span className="text-opacity-75   text-[#050038] md:text-sm">Unleash creative ideas and build on<br></br> them with the help of sticky notes, <br></br>images, mind maps, videos, drawing<br></br> capabilities — the list goes on.</span>
-                        </div>  </div>
-                    <LearnMore />
+            <div className="max-w-5xl mx-auto mt-12 py-8">
+                <h1 className="lg:text-5xl text-2xl lg:text-start  text-center text-[#050038] font-bold mb-4">Built for all kinds of teams</h1>
+                <div className="">
+                    <BuiltKindTeam></BuiltKindTeam>
                 </div>
-                <div className="lg:col-span-2 grid-cols-1 ">
-                    <img src={RightSideImage} alt="Brainstorming Image" className="rounded-lg" />
+                <div className="mt-8 grid  lg:grid-cols-3   gap-4">
+                    <div className="lg:col-span-1 grid-cols-1 justify-center">
+                        <div className='lg:text-start text-center text-[#050038] text-opacity-60 '>
+                            <ul className="list-disc text-sm ">
+                                <li className="flex items-center mb-5">
+                                    <FaCheck className="mr-5 text-[#050038]" />
+                                    Build low-fi wireframes
+                                </li>
+                                <li className="flex items-center mb-5">
+                                    <FaCheck className="mr-5 text-[#050038] " />
+                                    Involve stakeholders in the<br></br> design process
+                                </li>
+                                <li className="flex items-center mb-5">
+                                    <FaCheck className="mr-5 text-[#050038]" />
+                                    Run engaging design <br></br> workshops
+                                </li>
+                            </ul>
+                           </div>
+                        <LearnMore />
+                    </div>
+                    <div className="lg:col-span-2 grid-cols-1 ">
+                        <img src={RightSideImage} alt="Brainstorming Image" className="rounded-lg" />
+                    </div>
                 </div>
             </div>
-        </div>
-    </>
+        </>
     );
 };
 
