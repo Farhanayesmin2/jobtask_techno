8075b47e7c18ef501ddddc3b6dc2a55552f0fadc
Farhanayesmin2 <farhanayesmin550@gmail.com>
2024-03-05 20:16:06 +0600

    add the left side text

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -8,21 +8,29 @@ const Footer = () => {
 
         
           <div className="max-w-5xl mx-auto mt-12">
-    <div className="grid grid-cols-12 gap-4">
+    <div className="grid grid-cols-12 gap-4 py-20  ">
       <div className="col-span-4 ">
         <h1 className="text-white text-2xl"  >  Scan. Detect. Remove.</h1>
+       
+        <div className="">
 
-        <div className="mt-14">
 
-
-<h2 className="text-[#050038] lg:text-start text-center mb-5 text-opacity-60 text-sm"> Integrate your favorite tools</h2>
-<div className="flex gap-6  lg:items-start lg:justify-start items-center justify-center">
-<img src={fb} alt="Brainstorming Image" className="w-[26px] h-[26px]" />
-<img src={tw} alt="Brainstorming Image" className="w-[26px] h-[26px]" />
-<img src={yt} alt="Brainstorming Image" className="w-[32px] h-[28px]" />
+<h2 className="text-[#050038] lg:text-start text-center  text-opacity-60 text-sm"> Integrate your favorite tools</h2>
+<div className="flex gap-6 my-12 lg:items-start lg:justify-start items-center justify-center">
+<img src={fb} alt="Facebook Image" className="w-[36px] h-[36px]" />
+<img src={tw} alt="Twitter Image" className="w-[36px] h-[36px]" />
+<img src={yt} alt="Youtube Image" className="w-[36px] h-[36px]" />
 
 </div>
+<div className="flex gap-4 text-white text-xs ">
+          <a className="underline"  href="/privacy-policy">Privacy Policy</a>
+          <a className="underline"  href="/terms-of-service">Terms of Service</a>
+        </div>
 
+        <div className="text-xs my-4 text-white">
+          <span className="mb-3">Copyright © 2022 Certo Software Limited | Registered in England & Wales No. 10072356</span>
+          <p className="mt-3">Designed & developed by Bigger Picture</p>
+        </div>
 </div>
 
       </div>
